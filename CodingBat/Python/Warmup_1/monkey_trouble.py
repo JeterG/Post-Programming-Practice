d2c60d2120c78099e1d2f118133de3f41370673e
Jeter Gutierrez <jeter@pop-os.localdomain>
2019-09-11 05:59:10 -0400

    test

--- CodingBat/Python/Warmup_1/monkey_trouble.py
+++ CodingBat/Python/Warmup_1/monkey_trouble.py
@@ -3,4 +3,5 @@ def monkey_trouble(a_smile, b_smile):
     if (a_smile and b_smile) or (not a_smile and not b_smile):
         return True
     return False
+    
 print monkey_trouble(False,False)    
