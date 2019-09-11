a1824b5e55fa4f22602bdacba38c57f8c3f45c77
gutierrezjeter <gutierrezjeter@gmail.com>
2019-09-11 11:29:59 -0400

    near_hundred

--- CodingBat/Python/Warmup_1/near_hundred.py
+++ CodingBat/Python/Warmup_1/near_hundred.py
@@ -1,5 +1,5 @@
 #Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num) computes the aboslute value of a number.
 def near_hundred(n):
-    if (abs(100-n) or abs(200-n))<=10:
+    if abs(100-n)<=10 or abs(200-n) <= 10:
         return True
     return False
