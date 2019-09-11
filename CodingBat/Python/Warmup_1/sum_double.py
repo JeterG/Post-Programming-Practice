1e9f9aea8a33aaceca14b490592d95f72fd85936
gutierrezjeter <gutierrezjeter@gmail.com>
2019-09-11 06:06:14 -0400

    sum double

---
+++ CodingBat/Python/Warmup_1/sum_double.py
@@ -0,0 +1,8 @@
+#Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+def sum_double(a, b):
+    if a != b:
+        return a+b
+    else:
+        return a*a+b*b
+print sum_double(3,3)
+print sum_double(1,2)        
