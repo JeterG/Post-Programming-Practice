635096b1a3b4436ffe93047895609fded89fe779
gutierrezjeter <gutierrezjeter@gmail.com>
2019-09-11 06:00:24 -0400

    vs code error

--- CodingBat/Python/Warmup_1/monkey_trouble.py
+++ CodingBat/Python/Warmup_1/monkey_trouble.py
@@ -3,5 +3,6 @@ def monkey_trouble(a_smile, b_smile):
     if (a_smile and b_smile) or (not a_smile and not b_smile):
         return True
     return False
+
     
 print monkey_trouble(False,False)    
