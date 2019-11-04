bc6826492ecdba3d194cc112ae58dc1ebee6e607
gutierrezjeter <gutierrezjeter@gmail.com>
2019-11-03 20:34:29 -0500

    diff21

---
+++ CodingBat/Java/Warmup_1/diff21.java
@@ -0,0 +1,12 @@
+public class diff21{
+    // Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+    public int diff21(int n){
+        if (n>21){
+            int x= n -21;
+            return 2*x;
+        }
+        return 21-n;
+    }
+    public static void main(String[] args) {
+    }
+}
