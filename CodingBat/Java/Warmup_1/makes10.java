efc51aa35e108ffe178a13fcc9ce0819f51acea6
gutierrezjeter <gutierrezjeter@gmail.com>
2019-11-03 20:43:11 -0500

    makes10

---
+++ CodingBat/Java/Warmup_1/makes10.java
@@ -0,0 +1,12 @@
+public class makes10{
+    // Given 2 ints, a and b, return true if one of them is 10 or if their sum is 10.
+    public static  boolean makes10(int a, int b){
+        if ((a==10)||(b==10)||((a+b)==10)){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
