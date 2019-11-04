d05755a1719b48f19f5636904eb1959f52c020fc
gutierrezjeter <gutierrezjeter@gmail.com>
2019-11-03 20:30:43 -0500

    sumDouble

---
+++ CodingBat/Java/Warmup_1/sumDouble.java
@@ -0,0 +1,15 @@
+public class sumDouble{
+    
+    // Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+    public static int sumdouble(int a, int b){
+        if (a==b){
+            return 4*a;
+        }
+        else{
+            return a+b;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
