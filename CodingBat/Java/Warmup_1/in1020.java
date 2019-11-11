07e06cf677cce293f3405538ea26c3c613061aeb
gutierrezjeter <gutierrezjeter@gmail.com>
2019-11-11 15:09:53 -0500

    in1020

---
+++ CodingBat/Java/Warmup_1/in1020.java
@@ -0,0 +1,12 @@
+public class in1020{
+    public static boolean in1020(int a, int b){
+        //Given 2 int valiues, return true if either of them is in the range 10..20 inclusive.
+        if ((10<=a&&a<=20)||(10<=b&&b<=20)){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
