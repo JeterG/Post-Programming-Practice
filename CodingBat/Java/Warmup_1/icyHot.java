01139e9486f2295e9e768a0326a2a128478fdf9e
gutierrezjeter <gutierrezjeter@gmail.com>
2019-11-11 15:05:07 -0500

    icyHot

---
+++ CodingBat/Java/Warmup_1/icyHot.java
@@ -0,0 +1,10 @@
+public class icyHot{
+    public static boolean icyHot(int temp1, int temp2){
+    // Given two temperatures, return true if one is less than 0 and the other is greater than 100.
+        
+        return (temp1>100 && temp2<0)||(temp2>100 && temp1<0);
+    }
+    public static void main(String[] args) {
+        
+    }
+}
