9a9691cfaf4cb673cc385240fad0b8b43ff5f33e
gutierrezjeter <gutierrezjeter@gmail.com>
2019-11-11 15:15:39 -0500

    loneTeen

---
+++ CodingBat/Java/Warmup_1/loneTeen.java
@@ -0,0 +1,16 @@
+public class loneTeen{
+    
+    public static boolean loneTeen(int a, int b){
+        // We'll say that a number is "teen" if it is in the range 13..19 inclusive. Given 2 int values, return true if one or the other is teen but not both.
+        if (13<=a&&a<=19&&(b<13||b>19)){
+            return true;
+        }
+        if(13<=b&&b<=19&&(a<13||a>19)){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
