f44b9a6627beab4d3113494ec1df0e8e25728e54
gutierrezjeter <gutierrezjeter@gmail.com>
2019-11-11 15:34:48 -0500

    startOZ

---
+++ CodingBat/Java/Warmup_1/startOz.java
@@ -0,0 +1,19 @@
+public class startOz{
+    public static String startOz(String str){
+        // Given a string, return a string made of the first 2 chars (if present), however include the first char only if it is 'o' and iclude the second only if it is 'z' so "ozymandias" yields "oz".
+        if(str.length()<=0){
+            return str;
+        }
+        String temp="";
+        if(str.charAt(0)=='o'){
+            temp+='o';
+        }
+        if(str.charAt(1)=='z'){
+            temp+='z';
+        }
+        return temp;
+    }
+    public static void main(String[] args) {
+        System.out.println(startOz("oz"));
+    }
+}
