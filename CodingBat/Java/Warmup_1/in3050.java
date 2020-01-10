dfd6949b2563a1b642491469578ebf69baeefae2
gutierrezjeter <gutierrezjeter@gmail.com>
2020-01-10 02:02:27 -0500

    in3050

---
+++ CodingBat/Java/Warmup_1/in3050.java
@@ -0,0 +1,11 @@
+public class in3050{
+    
+    public static boolean in3050(int a, int b){
+        // Given 2 int values, return true if they are both in the range 30..50 incluse, or the yar eboth in the range 40..50 inclusive.
+        return ((a>=30)&&(a<=40)&&(b>=30)&&(b<=40))||((a>=40)&&(a<=50)&&(b>=40)&&(b<=50));
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
