af43fc6653e9ac52d68f47900676bd21b9d756f7
gutierrezjeter <gutierrezjeter@gmail.com>
2020-01-13 04:10:19 -0500

    makeOutWord

--- CodingBat/Java/String_1/String_1.java
+++ CodingBat/Java/String_1/String_1.java
@@ -19,10 +19,17 @@ public class String_1{
         return "<"+tag+">"+word+"</"+tag+">";
 
     }
+
+    public static String makeOutWord(String out, String word){
+        // Given an "out" string length 4, such as "<<>>", and a word, return a new string where the word is in the middle of the out string, e.g. "<<word>>". Note: use str.substring(i, j) to extract the String starting at index i and going up to but not including index j.
+
+        return out.substring(0,2)+word+out.substring(2);
+    }
     
     public static void main(String[] args) {
         System.out.println(helloName("Jeter"));
         System.out.println(makeAbba("Hi","Bye"));
         System.out.println(makeTags("i","Yay"));
+        System.out.println(makeOutWord("<<>>","Yay"));
     }
 }
