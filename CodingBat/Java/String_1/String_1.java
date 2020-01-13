e0cacb29254747618eda5bc0945bb413d5cb2dcc
gutierrezjeter <gutierrezjeter@gmail.com>
2020-01-13 04:29:34 -0500

    right2

--- CodingBat/Java/String_1/String_1.java
+++ CodingBat/Java/String_1/String_1.java
@@ -84,6 +84,11 @@ public class String_1{
         return str.substring(2)+str.substring(0,2);
     }
 
+    public static String right2(String str){
+        // Given a string, return a "rotated right 2" version where the last 2 chars are moved to the start. The string length will be at least 2.
+        return str.substring(str.length()-2)+str.substring(0,str.length()-2);
+    }
+
     public static void main(String[] args) {
         System.out.println(helloName("Jeter"));
         System.out.println(makeAbba("Hi","Bye"));
@@ -96,5 +101,6 @@ public class String_1{
         System.out.println(comboString("Hello","hi"));
         System.out.println(nonStart("Hello","There"));
         System.out.println(left2("Hello"));
+        System.out.println(right2("Hello"));
     }
 }
