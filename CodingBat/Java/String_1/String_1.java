cbc5da9af09c630bd5d7d7f909d0f917a3c484cc
gutierrezjeter <gutierrezjeter@gmail.com>
2020-01-13 13:53:42 -0500

    lastChars

--- CodingBat/Java/String_1/String_1.java
+++ CodingBat/Java/String_1/String_1.java
@@ -186,11 +186,30 @@ public class String_1{
         else{
             return str.substring(0,2);
         }
-        
+    }
+
+    public static String lastChars(String a, String b) {
+
+        // Given 2 strings, a and b, return a new string made of the first char of a and the last char of b, so "yo" and "java" yields "ya". If either string is length 0, use '@' for its missing char.
+
+        if((a.length()==0)&&(b.length()==0)){
+            return "@@";
+        }
+        else if(a.length()==0){
+            return "@"+b.substring(b.length()-1);
+        }
+        else if(b.length()==0){
+            return a.substring(0,1)+"@";
+        }
+        else{
+            return a.substring(0,1)+b.substring(b.length()-1);
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println(atFirst("h"));
+        // System.out.println(lastChars("last","chars"));
+        System.out.println(lastChars("hi",""));
+        // System.out.println(atFirst("h"));
         // System.out.println(hasBad("xbadxx"));
         // System.out.println(middleThree("Candy"));
         // System.out.println(helloName("Jeter"));
