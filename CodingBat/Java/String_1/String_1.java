c9e7e30f60271b1c2ef3ee208e853aff99d03254
gutierrezjeter <gutierrezjeter@gmail.com>
2020-01-13 04:01:20 -0500

    helloName

---
+++ CodingBat/Java/String_1/String_1.java
@@ -0,0 +1,12 @@
+public class String_1{
+    
+    public static String helloName(String name){
+        // Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+        return "Hello "+name +"!";
+    }
+
+    
+    public static void main(String[] args) {
+        System.out.println(helloName("Jeter"));
+    }
+}
