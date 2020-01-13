02bf515ed5167d4c4b93ce8d92be2acaf6c617b2
gutierrezjeter <gutierrezjeter@gmail.com>
2020-01-13 04:06:50 -0500

    makeTags

--- CodingBat/Java/String_1/String_1.java
+++ CodingBat/Java/String_1/String_1.java
@@ -11,10 +11,18 @@ public class String_1{
         return a+b+b+a;
 
     }
+    
+    public static String makeTags(String tag, String word){
 
+        // The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i" tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with tags around the word, e.g. "<i>Yay</i>".
+
+        return "<"+tag+">"+word+"</"+tag+">";
+
+    }
     
     public static void main(String[] args) {
         System.out.println(helloName("Jeter"));
         System.out.println(makeAbba("Hi","Bye"));
+        System.out.println(makeTags("i","Yay"));
     }
 }
