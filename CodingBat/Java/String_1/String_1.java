772a0828c87b10bd3546ee15695e5cae95347ad6
gutierrezjeter <gutierrezjeter@gmail.com>
2020-01-13 14:21:09 -0500

    lastTwo

--- CodingBat/Java/String_1/String_1.java
+++ CodingBat/Java/String_1/String_1.java
@@ -220,8 +220,23 @@ public class String_1{
         }
     }
 
+    public static String lastTwo(String str) {
+        // Given a string of any length, return a new string where the last 2 chars, if present, are swapped, so "coding" yields "codign".
+        if(str.length()<=1){
+            return str;
+        }
+        else if(str.length()==2){
+            return str.substring(1)+str.substring(0,1);
+
+        }
+        else{
+            return str.substring(0,str.length()-2)+str.charAt(str.length()-1)+str.charAt(str.length()-2);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(conCat("abc","cat"));
+        System.out.println(lastTwo("coding"));
+        // System.out.println(conCat("abc","cat"));
         // System.out.println(lastChars("last","chars"));
         // System.out.println(lastChars("hi",""));
         // System.out.println(atFirst("h"));
