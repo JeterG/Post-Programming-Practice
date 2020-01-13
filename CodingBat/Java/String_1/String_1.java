db04cccfb9d14c97c46abc9a4125784209461f27
gutierrezjeter <gutierrezjeter@gmail.com>
2020-01-13 04:23:33 -0500

    comboString

--- CodingBat/Java/String_1/String_1.java
+++ CodingBat/Java/String_1/String_1.java
@@ -65,6 +65,15 @@ public class String_1{
 
     }
 
+
+    public static String comboString(String a,String b){
+        // Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length 0).
+        if(a.length()>b.length()){
+            return b+a+b;
+        }
+        return a+b+a;
+    }
+
     public static void main(String[] args) {
         System.out.println(helloName("Jeter"));
         System.out.println(makeAbba("Hi","Bye"));
@@ -74,5 +83,6 @@ public class String_1{
         System.out.println(firstTwo("Hello"));
         System.out.println(firstHalf("WooHoo"));
         System.out.println(withoutEnd("Hello"));
+        System.out.println(comboString("Hello","hi"));
     }
 }
