63671fc5e597f5c14a1e9c0dd9de6559b7ad987a
gutierrezjeter <gutierrezjeter@gmail.com>
2020-01-13 04:03:27 -0500

    makeAbba

--- CodingBat/Java/String_1/String_1.java
+++ CodingBat/Java/String_1/String_1.java
@@ -5,8 +5,16 @@ public class String_1{
         return "Hello "+name +"!";
     }
 
+    public static String makeAbba(String a, String b){
+
+        // Given two strings, a and b, return the result of putting them together in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+        return a+b+b+a;
+
+    }
+
     
     public static void main(String[] args) {
         System.out.println(helloName("Jeter"));
+        System.out.println(makeAbba("Hi","Bye"));
     }
 }
