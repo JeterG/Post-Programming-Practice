b337e43568c6a20766d05d0424baa6ba1a69f370
gutierrezjeter <gutierrezjeter@gmail.com>
2020-01-13 14:47:41 -0500

    minCat

--- CodingBat/Java/String_1/String_1.java
+++ CodingBat/Java/String_1/String_1.java
@@ -264,8 +264,21 @@ public class String_1{
         return(str.substring(0,2).equals(str.substring(str.length()-2)));
     }
 
+    public static String minCat(String a, String b) {
+        //Given two strings, append them together (known as "concatenation") and return the result. However, if the strings are different lengths, omit chars from the longer string so it is the same length as the shorter string. So "Hello" and "Hi" yield "loHi". The strings may be any length.
+        if((a.length()==0)||(b.length()==0)){
+            return "";
+        }
+        else if(a.length()<b.length()){
+            return a+b.substring(b.length()-a.length());
+        }
+        return a.substring(a.length()-b.length())+b;
+    }
+
     public static void main(String[] args) {
-        System.out.println(frontAgain("edited"));
+        System.out.println(minCat("Hello","Hi"));
+        System.out.println(minCat("Hello","java"));
+        // System.out.println(frontAgain("edited"));
         // System.out.println(seeColor("bluexx"));
         // System.out.println(lastTwo("coding"));
         // System.out.println(conCat("abc","cat"));
