02507ca948d77bb8c0b181e3e236b22bb30db9a1
gutierrezjeter <gutierrezjeter@gmail.com>
2020-01-20 03:13:10 -0500

    withoutX2

--- CodingBat/Java/String_1/String_1.java
+++ CodingBat/Java/String_1/String_1.java
@@ -402,8 +402,51 @@ public class String_1{
         return str;
     }
 
+    public static String withoutX2(String str) {
+        // Given a string, if one or both of the first 2 chars is 'x', return the string without those 'x' chars, and otherwise return the string unchanged. This is a little harder than it looks.
+        if(str.length()==0){
+            return "";
+        }
+        else if(str.length()==1){
+            if (str.equals("x")){
+                return "";
+            }
+            return str;
+        }
+        else if(str.length()==2){
+            if(str.charAt(0)=='x'){
+                if(str.charAt(1)=='x'){
+                    return "";
+                }
+                return str.substring(1);
+            }
+            if(str.charAt(1)=='x'){
+                if(str.charAt(0)=='x'){
+                    return "";
+                }
+                return str.substring(0,1);
+            }
+        }
+        else{
+            if(str.charAt(0)=='x'){
+                if(str.charAt(1)=='x'){
+                    return str.substring(2);
+                }
+                return str.substring(1);
+            }
+            if(str.charAt(1)=='x'){
+                if(str.charAt(0)=='x'){
+                    return str.substring(2);
+                }
+                return str.substring(0,1)+str.substring(2);
+            }
+        }
+        return str;
+    }
+
     public static void main(String[] args) {
-        System.out.println(withoutX("Hxix"));
+        System.out.println(withoutX2("Hxi"));
+        // System.out.println(withoutX("Hxix"));
         // System.out.println(startWord("hippo","hi"));
         // System.out.println(startWord("hippo","xip"));
         // System.out.println(startWord("hippo","i"));
