2df9936e3d25533a012acd15f35d52e053fc129c
gutierrezjeter <gutierrezjeter@gmail.com>
2020-01-20 03:28:34 -0500

    firstLast6

---
+++ CodingBat/Java/Array_1/Array_1.java
@@ -0,0 +1,14 @@
+public class Array_1{
+    public static boolean firstLast6(int[] nums) {
+        // Given an array of ints, return true if 6 appears as either the first or last element in the array. The array will be length 1 or more.
+        if(nums.length==1){
+            return (nums[0]==6);
+        }
+        return((nums[0]==6)||(nums[nums.length-1]==6));
+
+    }
+    public static void main(String[] args) {
+        int[] a= new int[]{1,2,6};
+        System.out.println(firstLast6(a));
+    }
+}
