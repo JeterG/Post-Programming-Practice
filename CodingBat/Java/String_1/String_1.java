372b8372e4ebdaa6ebcd3f0f31df02b1116d85e7
gutierrezjeter <gutierrezjeter@gmail.com>
2020-01-20 02:53:08 -0500

    startWord

--- CodingBat/Java/String_1/String_1.java
+++ CodingBat/Java/String_1/String_1.java
@@ -342,17 +342,28 @@ public class String_1{
 
     public static String startWord(String str, String word) {
         // Given a string and a second "word" string, we'll say that the word matches the string if it appears at the front of the string, except its first char does not need to match exactly. On a match, return the front of the string, or otherwise return the empty string. So, so with the string "hippo" the word "hi" returns "hi" and "xip" returns "hip". The word will be at least length 1.
-
-        if (word.length()==1){
-            return word;
-        }
-        else if(word.length()==2){
-            if 
-        }
-        
+       int endWord=word.length();
+       int startWord=1;
+       String matchWord=word.substring(startWord,endWord);
+       if(str.length()==0){
+           return "";
+       }
+       if(str.length()<word.length()){
+           return "";
+       }
+       String strMatch=str.substring(startWord,endWord);
+       if(strMatch.equals(matchWord)){
+           return str.substring(0,1)+strMatch;
+       }
+       else{
+           return "";
+       }
     }
 
     public static void main(String[] args) {
+        System.out.println(startWord("hippo","hi"));
+        System.out.println(startWord("hippo","xip"));
+        System.out.println(startWord("hippo","i"));
         // System.out.println(deFront("Hello"));
         // System.out.println(deFront("away"));
         // System.out.println(without2("HelloHe"));
