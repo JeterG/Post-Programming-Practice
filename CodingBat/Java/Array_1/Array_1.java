16f30f62f2595fe028035af91ca3d469d020b14e
jeterg <jetergdev@gmail.com>
2020-02-07 20:07:07 -0500

    sameFirstLast

--- CodingBat/Java/Array_1/Array_1.java
+++ CodingBat/Java/Array_1/Array_1.java
@@ -7,8 +7,16 @@ public class Array_1{
         return((nums[0]==6)||(nums[nums.length-1]==6));
 
     }
+    public static boolean sameFirstLast(int[] nums){
+        // Given an array of ints, return true if the array length is 1 or more, and the first element and the last element are equal
+        if(nums.length<1){
+            return false;
+        }
+        return (nums[0]==nums[nums.length-1]);
+    }
     public static void main(String[] args) {
-        int[] a= new int[]{1,2,6};
+        int[] a= new int[]{1,2,1};
         System.out.println(firstLast6(a));
+        System.out.println(sameFirstLast(a));
     }
 }
