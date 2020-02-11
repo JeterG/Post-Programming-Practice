47a0b04357374ecf6af9ff63e652d3acb5fb042e
jeterg <jetergdev@gmail.com>
2020-02-10 21:34:30 -0500

    sum2

--- CodingBat/Java/Array_1/Array_1.java
+++ CodingBat/Java/Array_1/Array_1.java
@@ -52,6 +52,16 @@ public class Array_1{
         }
         return new int[] {nums[0], nums[0], nums[0]};
     } 
+    public static int sum2(int[] nums){
+        // Given an array of ints, return the sum of hte first 2 elements in the array. If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0
+        if (nums.length==0){
+            return 0;
+        }
+        if (nums.length==1){
+            return nums[0];
+        }
+        return nums[0]+nums[1];
+    }
     public static void main(String[] args) {
         int[] a= new int[]{1,2,3};
         System.out.println(firstLast6(a));
