85c5400e17b386617eff2de1db34887b12d73896
jeterg <jetergdev@gmail.com>
2020-02-10 21:39:16 -0500

    make ends

--- CodingBat/Java/Array_1/Array_1.java
+++ CodingBat/Java/Array_1/Array_1.java
@@ -66,6 +66,10 @@ public class Array_1{
         // Given 2 int arrays, a and b, each length 3, return a new array length 2 containing their middle elements.
         return new int[]{a[1],b[1]};
     }
+    public static int[] makeEnds(int[] nums){
+        // Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more.
+        return new int[]{nums[0],nums[nums.length-1]};
+    }
     public static void main(String[] args) {
         int[] a= new int[]{1,2,3};
         System.out.println(firstLast6(a));
