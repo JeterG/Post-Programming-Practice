00b6fa6d96ffc72f9b0bde8d044a1a6d950cfcb4
jeterg <jetergdev@gmail.com>
2020-03-29 14:40:12 -0400

    unlucky1

--- CodingBat/Java/Array_1/Array_1.java
+++ CodingBat/Java/Array_1/Array_1.java
@@ -217,6 +217,24 @@ public class Array_1{
         }
         else{
             return new int[]{nums[0],nums[1]};
+        }   
+    }
+    public static boolean unlucky1(int[] nums) {
+        // We'll say that a 1 immediately followed by a 3 in an array is an "unlucky" 1. Return true if the given array contains an unlucky 1 in the first 2 or last 2 positions in the array.
+        if(nums.length<=1){
+            return false;
+        }
+        else if(nums[0]==1&&nums[1]==3){
+            return true;
+        }
+        else if(nums[1]==1&&nums[2]==3){
+            return true;
+        }
+        else if(nums[nums.length-1]==3&&nums[nums.length-2]==1){
+            return true;
+        }
+        else{
+            return false;
         }
         
     }
