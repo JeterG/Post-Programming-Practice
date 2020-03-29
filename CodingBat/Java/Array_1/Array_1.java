40b8d234b0df68b63d449942626e1cdfd5535891
jeterg <jetergdev@gmail.com>
2020-03-29 14:47:51 -0400

    Array1

--- CodingBat/Java/Array_1/Array_1.java
+++ CodingBat/Java/Array_1/Array_1.java
@@ -254,6 +254,23 @@ public class Array_1{
         }
         
     }
+
+    public static int[] front11(int[] a, int[] b) {
+        // Given 2 int arrays, a and b, of any length, return a new array with the first element of each array. if either array is length 0, ignore that array.
+        if(a.length==0&&b.length==0){
+            return new int[]{};
+        }
+        else if(a.length==0){
+            return new int[] {b[0]};
+        }
+        else if(b.length==0){
+            return new int[] {a[0]};
+        }
+        else{
+            return new int[] {a[0],b[0]};
+        }
+        
+    }
     public static void main(String[] args) {
         int[] a= new int[]{1,2,3};
         System.out.println(firstLast6(a));
