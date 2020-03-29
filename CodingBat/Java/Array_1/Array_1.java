d36d7d1f8444fbf0dba88e186195fd62679be4b7
jeterg <jetergdev@gmail.com>
2020-03-29 14:45:15 -0400

    make2

--- CodingBat/Java/Array_1/Array_1.java
+++ CodingBat/Java/Array_1/Array_1.java
@@ -238,6 +238,22 @@ public class Array_1{
         }
         
     }
+    public static int[] make2(int[] a,int[] b) {
+        // Given 2 int arrays, a and b, return a new array lenght 2 containing, as much as will fit, the elements from a followed by the elements from b. The arrays may e any length, including 0, but there will be 2 or more elements available between the 2 arrays.
+        if(a.length==0){
+            return new int[] {b[0],b[1]};
+        }
+        else if(a.length==1){
+            return new int[] {a[0],b[0]};
+        }
+        else if(a.length==2){
+            return a;
+        }
+        else {
+            return new int[] {a[0],a[1]};
+        }
+        
+    }
     public static void main(String[] args) {
         int[] a= new int[]{1,2,3};
         System.out.println(firstLast6(a));
