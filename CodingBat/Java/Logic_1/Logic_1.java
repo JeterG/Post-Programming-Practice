e92b8f5ef309f6cc320147ee762e29ef544da504
jeterg <jetergdev@gmail.com>
2020-03-30 09:54:53 -0400

    love6

--- CodingBat/Java/Logic_1/Logic_1.java
+++ CodingBat/Java/Logic_1/Logic_1.java
@@ -95,6 +95,17 @@ public class Logic_1{
         return "off";
 
     }
+
+    public static boolean love6(int a, int b){
+        // The number 6 is a truly great number. Given two int values, a and b, return true if either one is 6. Or if their sum or difference is 6. Note: the function Math.abs(num) computes the absolute value of a number.
+        if(a==6||b==6){
+            return true;
+        }
+        else if(Math.abs(a-b)==6){
+            return true;
+        }
+        return (a+b==6);
+    }
     public static void main(String[] args) {
         System.out.println("Test");
     }
