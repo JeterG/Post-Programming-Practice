bc9d2ad6ca191a51bc6d72b15734fe7824da8799
jeterg <jetergdev@gmail.com>
2020-06-14 14:59:05 -0400

    endother

--- CodingBat/Java/String_2/String_2.java
+++ CodingBat/Java/String_2/String_2.java
@@ -53,15 +53,38 @@ public class String_2 {
             }
         }
                 return count;
+    }
+    
+    public boolean endOther(String a, String b){
+        // Given two strings, return true if either of the strings appears at the very end of the other string, ignoring upper/lower case differences (in other words, the computation should not be "case sensitive"). Note: str.toLowerCase() returns the lowercase version of a string.
+
+        if(a.length()==b.length()){
+            return a.toLowerCase().equals(b.toLowerCase());
+        }
+        else if(a.length()<b.length()){
+            return b.substring(b.length()-a.length()).toLowerCase().equals(a.toLowerCase());
+        }
+        else{
+            return a.substring(a.length()-b.length()).toLowerCase().equals(b.toLowerCase());
+        }
+
 
     }
 
     public static void main(String[] args) {
         String_2 obj = new String_2();
-        System.out.println(obj.doubleChar("The"));
-        System.out.println(obj.countHi("abc hi hi"));
-        System.out.println(obj.catDog("1cat1cadodog"));
-        System.out.println(obj.countCode("cozexxcope"));
+        // System.out.println(obj.doubleChar("The"));
+        // System.out.println(obj.countHi("abc hi hi"));
+        // System.out.println(obj.catDog("1cat1cadodog"));
+        // System.out.println(obj.countCode("cozexxcope"));
+        System.out.println(obj.endOther("abc", "abXabc"));
+        // System.out.println("abXabc".length());
+        // System.out.println("abc".length());
+        // System.out.println("abXabc".length()-"abc".length());
+        // System.out.println("abXabc".substring("abXabc".length()-"abc".length()));
+
+
+
     }
 
 }
