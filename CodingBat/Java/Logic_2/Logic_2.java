0a43d2519dc3a6c553bb1a67f7834f84fa0a995d
jeterg <jetergdev@gmail.com>
2020-06-14 13:59:41 -0400

    makeChocolate

--- CodingBat/Java/Logic_2/Logic_2.java
+++ CodingBat/Java/Logic_2/Logic_2.java
@@ -1,3 +1,5 @@
+import java.util.Arrays; 
+
 public class Logic_2{
 
         public static boolean makeBricks(int small, int big, int goal){
@@ -112,8 +114,31 @@ public class Logic_2{
         int[ ] arr={a,b,c};
         Arrays.sort(arr);
         return (arr[0]-arr[1]==arr[1]-arr[2]);
-
     }
+    
+    public static int makeChocolate(int small, int big, int goal){
+    // We want make a package of goal kilos of chocolate. We have small bars(1 kilo each) and big bars (5 kilos each). Return the number of small bars to use, assuming we always use big bars before small bars. Return -1 if it can't be done.
+    if (small + big*5<goal){
+        return -1;
+    }
+    else if (big*5+small==goal){
+        return small;}
+    else if (goal%5==small){
+        return small;}
+    else if ((goal%5==0) && (big*5>=goal)){
+        return 0;}
+    else if ((goal-big*5<small) && (goal-big*5>0)){
+        return goal-big*5;}    
+    else if (goal%5<small){
+        return goal%5;}   
+
+    return -1;
+    }
+
+
+
+
+
         public static void main(String[] args) {
 
             System.out.println(makeBricks(0,0,0));
